3be8a146308d05aeb9291e8937c02998135fbb22
Luke van der Hoeven <hungerandthirst@gmail.com>
2015-11-06 18:10:16 -0500

    command interface

--- endpoints/slack_command_api.rb
+++ endpoints/slack_command_api.rb
@@ -1,28 +1,42 @@
+require_relative '../models/chompy_command'
+
 module Chompy
   class SlackCommandAPI < Roda
     plugin :json
 
+    repo = Statuses.new
+
     route do |r|
       r.on 'chompy' do
         r.post do
-          { text: 'Invalid command configuration' } unless valid_token?(r.params['token'])
+          cmd = ChompyCommand.new(r.params)
 
-          user_id = r.params['user_id']
-          name = r.params['user_name']
-          command = r.params['text']
+          if cmd.valid_token?
+            status = repo.toggle(cmd.user_id)
 
-          {
-            response_type: "in_channel",
-            text: "#{name} be like :chompy:"
-          }
+            {
+              response_type: "in_channel",
+              text: away?(status) ?  away_msg(cmd) : back_msg(cmd)
+            }
+          else
+            { text: 'Invalid chompy configuration' }
+          end
         end
       end
     end
 
     private
 
-    def valid_token?(token)
-      token == ENV.fetch('SLASH_COMMAND_TOKEN')
+    def away?(status)
+      (status == 'OK')
+    end
+
+    def away_msg(cmd)
+      "#{cmd.slack_name} is :chompy:"
+    end
+
+    def back_msg(cmd)
+      "#{cmd.slack_name} is back"
     end
   end
 end
---
+++ models/chompy_command.rb
@@ -0,0 +1,19 @@
+module Chompy
+  class ChompyCommand
+    extend Forwardable
+
+    def_delegators :@command, :user_id, :user_name, :token
+
+    def initialize(req)
+      @command = OpenStruct.new(req)
+    end
+
+    def slack_name
+      "<@#{user_id}|#{user_name}>"
+    end
+
+    def valid_token?
+      token == ENV.fetch('SLASH_COMMAND_TOKEN')
+    end
+  end
+end
